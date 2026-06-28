4ac61c1567e22fe76ac0baa20cf2847c6a6f84b3
MS893 <ms893@free.fr>
2026-06-28 23:29:02 +0200

    Déploiement Perfos Web avec sqlite

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2571101734" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4070242750" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
